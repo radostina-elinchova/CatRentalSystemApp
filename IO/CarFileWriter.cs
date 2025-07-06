a9fb881a0a71819fc1f5eb017401ef8f74a7b75f
radostina-elinchova <radostina.elinchova@gmail.com>
2025-07-06 12:48:25 +0300

    Addint IO classes

---
+++ IO/CarFileWriter.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.IO;
+using CarRentalSystemApp.Models;
+using CarRentalSystemApp.Interfaces;
+
+namespace CarRentalSystemApp.IO
+{
+    public class CarFileWriter : ICsvWriter
+    {
+        private readonly string filePath;
+
+        public CarFileWriter(string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+        public void WriteCars(List<Car> cars)
+        {
+            try
+            {
+                using (var writer = new StreamWriter(filePath))
+                {
+                    writer.WriteLine("Id,Make,Model,Year,Type,Status,CurrentRenter");
+                    foreach (var car in cars)
+                    {
+                        writer.WriteLine(car.ToString());
+                    }
+                }
+
+                Console.WriteLine("Cars saved successfully to CSV.");
+                Console.WriteLine($"[DEBUG] Saved {cars.Count} cars to '{Path.GetFullPath(filePath)}'");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to save cars: {ex.Message}");
+            }
+        }
+    }
+}
